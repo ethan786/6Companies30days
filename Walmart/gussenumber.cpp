1b777fa0341019e867c4249827f05ab25e49ce31
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-31 19:38:28 +0530

    Create gussenumber.cpp

---
+++ Walmart/gussenumber.cpp
@@ -0,0 +1,16 @@
+vector<pair<int, int>> ess(n);
+        for (int i = 0; i < n; ++i)
+            ess[i] = {efficiency[i], speed[i]};
+        sort(rbegin(ess), rend(ess));
+        long sumS = 0, res = 0;
+        priority_queue <int, vector<int>, greater<int>> pq; //min heap
+        for(auto& [e, s]: ess){
+            pq.emplace(s);
+            sumS += s;
+            if (pq.size() > k) {
+                sumS -= pq.top();
+                pq.pop();
+            }
+            res = max(res, sumS * e);
+        }
+        return res % (int)(1e9+7);
